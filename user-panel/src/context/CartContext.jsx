11468edf8fdecc9abc2496f54167fe3bbe989ee8
Berserker <155231526+Berserker-k01@users.noreply.github.com>
2026-01-13 18:20:40 +0000

    Update CartContext.jsx

--- user-panel/src/context/CartContext.jsx
+++ user-panel/src/context/CartContext.jsx
@@ -16,6 +16,7 @@ export const CartProvider = ({ children, initialPixelId }) => {
         }
     })
 
+    const [isCartOpen, setIsCartOpen] = useState(false)
     const [facebookPixelId, setFacebookPixelId] = useState(initialPixelId)
 
     useEffect(() => {
